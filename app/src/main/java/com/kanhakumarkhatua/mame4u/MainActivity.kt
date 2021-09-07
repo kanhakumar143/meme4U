a03e25a42421ad635bbfbdbee2737789da8df1b3
kanhakumar143 <kanhakumarkhatua2001@gmail.com>
2021-09-07 04:09:56 -0700

    Update Something

--- app/src/main/java/com/kanhakumarkhatua/mame4u/MainActivity.kt
+++ app/src/main/java/com/kanhakumarkhatua/mame4u/MainActivity.kt
@@ -2,26 +2,25 @@ package com.kanhakumarkhatua.mame4u
 
 import android.content.Intent
 import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
-
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
-import com.kanhakumarkhatua.mame4u.MySingleton.MySingleton.Companion.getInstance
 
 
 class MainActivity : AppCompatActivity() {
 
     var currentImgUrl: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +42,7 @@ class MainActivity : AppCompatActivity() {
             { response ->
                 currentImgUrl = response.getString("url")
                 val imageView = findViewById<ImageView>(R.id.memeImgView)
+
                 Glide.with(this).load(currentImgUrl).listener(object: RequestListener<Drawable>{
                     override fun onLoadFailed(
                         e: GlideException?,
@@ -81,6 +81,7 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra(Intent.EXTRA_TEXT, "Hii, Checkout the image $currentImgUrl")
         val chooser = Intent.createChooser(intent, "Share meme using ...")
         startActivity(chooser)
+
     }
     fun nextMeme(view: View) {
         loadMeme()
